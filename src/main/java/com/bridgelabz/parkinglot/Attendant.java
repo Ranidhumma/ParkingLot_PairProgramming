bec34893c22f0da2674c948f5513d19fc0de6217
KarthikBenki <karthikmc007@gmail.com>
2022-04-30 14:07:04 +0530

    Refactored key generation from owner to attendant

--- src/main/java/com/bridgelabz/parkinglot/Attendant.java
+++ src/main/java/com/bridgelabz/parkinglot/Attendant.java
@@ -3,11 +3,35 @@ package com.bridgelabz.parkinglot;
 import java.util.Map;
 
 public class Attendant {
-    ParkingLotOwner owner = new ParkingLotOwner();
+    private int key=0;
 
+    public int parkThevehicle(Map<Integer, Vehicle> parkingLotMap, DriverType driverType) {
+//        for(Map.Entry map : parkingLotMap.entrySet()){
+//            if(map.getValue()==null) {
+//                this.key = (int) map.getKey();//finds the key where the lot is empty
+//                break;//breaks after finding evenly parked key
+//            }
+//        }
+        if (driverType == DriverType.NORMAL) {
+            int normalKey = 6;
+            for ( normalKey = 6; normalKey <= parkingLotMap.size(); normalKey++) {
+                if (parkingLotMap.get(normalKey) == null)
+                    return normalKey;
+            }
+        }
+        if (driverType == DriverType.HANDICAP)
+            for (int key = 1; key <= parkingLotMap.size(); key++) {
+                if (parkingLotMap.get(key) == null)
+                    return key;
+            }
 
-    public int parkThevehicle(Map<Integer, Vehicle> parkingLotMap) {
-        return owner.getKeyToPark(parkingLotMap);
-
+        if (driverType == DriverType.NORMAL) {
+            int normalKey = 1;
+            for ( normalKey=1; normalKey <= parkingLotMap.size(); normalKey++) {
+                if (parkingLotMap.get(normalKey) == null)
+                    return normalKey;
+            }
+        }
+        return this.key;
     }
 }
